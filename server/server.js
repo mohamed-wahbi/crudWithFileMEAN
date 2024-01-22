e8e24a0ccc0f456d07c0d7d2b18803ab223a1979
Med Wahbi <wahbisj@gmail.com>
2024-01-22 12:12:59 +0100

    instalation du serveur sur le port 5000

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require ('express') ;
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT;
+
+
+
+
+app.listen(port,()=>{
+    console.log('Server is active *_*');
+})
