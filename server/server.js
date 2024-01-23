2af246bb6d876e741cbcd13139525826f2ef9eab
Med Wahbi <wahbisj@gmail.com>
2024-01-23 11:43:16 +0100

    projet initialiser <angular> .

--- server/server.js
+++ server/server.js
@@ -6,18 +6,12 @@ const port = process.env.PORT;
 const cors = require("cors");
 
 const articleRoute = require("./routes/articleRoute");
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/articles", articleRoute);
-app.use("/getImage", express.static('./upload'));
+app.use("/getImage", express.static("./upload"));
 
-
-// Server setup
 app.listen(port, () => {
   console.log("Server is active *_*");
 });
-
